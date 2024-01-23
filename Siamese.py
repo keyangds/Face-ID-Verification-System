bf04519ec37d9c9cd46ed3e6431262a3d2d8c435
Keyang Xuan <107345948+keyangds@users.noreply.github.com>
2024-01-23 13:00:45 -0600

    Add files via upload

---
+++ Siamese.py
@@ -0,0 +1,80 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+class SiameseNetwork(nn.Module):
+    def __init__(self, in_channels):
+        super(SiameseNetwork, self).__init__()
+        # Setting up the Sequential of CNN Layers
+        self.cnn1 = nn.Sequential(
+            nn.Conv2d(in_channels, 16, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(5,alpha=0.0001,beta=0.75,k=2),
+            nn.MaxPool2d(2),
+            
+            nn.Conv2d(16, 32, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(5,alpha=0.0001,beta=0.75,k=2),
+            nn.MaxPool2d(2),
+            nn.Dropout2d(p=0.3),
+
+            nn.Conv2d(32,32 ,3,padding=1),
+            nn.ReLU(inplace=True),
+            
+            nn.Conv2d(32,64,3,padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, stride=2),
+            nn.Dropout2d(p=0.3),
+        )
+
+        # Defining the fully connected layers
+        self.fc1 = nn.Sequential(
+            nn.Linear(3136, 1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5),
+            
+            nn.Linear(1024, 128),
+            nn.ReLU(inplace=True),
+            
+            nn.Linear(128,2))
+        
+    def forward_once(self, x):
+        # Forward pass 
+        x = x.reshape(1,1,64,64)
+        output = self.cnn1(x)
+        output = output.view(output.size()[0], -1)
+        output = self.fc1(output)
+        return output
+
+    def forward(self, input1, input2):
+        # forward pass of input 1
+        output1 = self.forward_once(input1)
+        # forward pass of input 2
+        output2 = self.forward_once(input2)
+        return output1, output2
+    
+
+
+class ContrastiveLoss(torch.nn.Module):
+    """
+    Contrastive loss function.
+    Based on:
+    """
+
+    def __init__(self, margin=1.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, x0, x1, y):
+        # euclidian distance
+        diff = x0 - x1
+        dist_sq = torch.sum(torch.pow(diff, 2), 1)
+        dist = torch.sqrt(dist_sq)
+        mdist = self.margin - dist
+        dist = torch.clamp(mdist, min=0.0)
+
+        loss = y * dist_sq + (1 - y) * torch.pow(dist, 2)
+        loss = torch.sum(loss) / 2.0 / x0.size()[0]
+  
+        return loss
